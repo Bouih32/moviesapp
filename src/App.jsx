ad48b8ef64c8dbc17c243e9c0a90a6d750595fed
Bouih32 <enamto.bouih@gmail.com>
2023-12-12 21:38:53 +0100

    added slick carousal

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 // import { FaRegBookmark, FaBookmark ,FaBookOpen,FaUserCircle,FaSearch} from "react-icons/fa";
 // import { TbMovie } from "react-icons/tb";
 // import { MdMovie } from "react-icons/md";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import PopularBanner from "./components/PopularBanner";
 import Header from "./components/Header";
@@ -9,6 +12,13 @@ import Search from "./components/Search";
 import Movie from "./components/Movie";
 
 function App() {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2.4,
+    slidesToScroll: 1,
+  };
   return (
     <div className="container">
       <Header />
@@ -16,11 +26,13 @@ function App() {
         <Search />
         <h1>Trending</h1>
         <div className="popularMovies">
-          <PopularBanner />
-          <PopularBanner />
-          <PopularBanner />
-          <PopularBanner />
-          <PopularBanner />
+          <Slider {...settings}>
+            <PopularBanner />
+            <PopularBanner />
+            <PopularBanner />
+            <PopularBanner />
+            <PopularBanner />
+          </Slider>
         </div>
         <h1>Recommended for you</h1>
         <div className="movies">
