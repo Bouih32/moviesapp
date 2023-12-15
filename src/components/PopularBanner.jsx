06769335b2481678f7f0a140965347efd850336f
Bouih Othmane <117870166+Bouih32@users.noreply.github.com>
2023-12-15 21:08:30 +0100

    Update PopularBanner.jsx

--- src/components/PopularBanner.jsx
+++ src/components/PopularBanner.jsx
@@ -9,16 +9,16 @@ export default function PopularBanner() {
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 2000 },
+      breakpoint: { max: 4000, min: 3000 },
       items: 5,
     },
     desktop: {
-      breakpoint: { max: 2000, min: 1024 },
-      items: 2.4,
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4.2,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      items: 2.4,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
