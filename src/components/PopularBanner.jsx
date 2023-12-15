484e51ff9f119d69e2fbdc68352e0d4574daf1a2
Bouih32 <enamto.bouih@gmail.com>
2023-12-15 20:58:07 +0100

    fiked slider

--- src/components/PopularBanner.jsx
+++ src/components/PopularBanner.jsx
@@ -1,39 +1,61 @@
 import Popular from "./Popular";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import { useOutletContext } from "react-router-dom";
 
 export default function PopularBanner() {
   const { info, setInfo } = useOutletContext();
-
-  var settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 2.4,
-    slidesToScroll: 1,
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 2.4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1.3,
+    },
   };
-
   return (
     <>
       <h1>Trending</h1>
-      <Slider {...settings}>
-        {info &&
-          info.map((thing) => {
-            return (
-              <Popular
-                key={thing.id}
-                name={thing.name}
-                poster={thing.poster}
-                date={thing.date}
-                id={thing.id}
-                setInfo={setInfo}
-                marked={thing.marked}
-              />
-            );
-          })}
-      </Slider>
+
+      {info && (
+        <Carousel
+          responsive={responsive}
+          autoPlay={true}
+          swipeable={true}
+          draggable={true}
+          showDots={false}
+          infinite={true}
+          partialVisible={false}
+          dotListClass="custom-dot-list-style"
+        >
+          {info &&
+            info.map((thing) => {
+              return (
+                <Popular
+                  key={thing.id}
+                  name={thing.name}
+                  poster={thing.poster}
+                  date={thing.date}
+                  id={thing.id}
+                  setInfo={setInfo}
+                  marked={thing.marked}
+                />
+              );
+            })}
+        </Carousel>
+      )}
     </>
   );
 }
